d825f98b2adc28ad79b5312548d96e1481bf91ba
Ali Eren Kayhan <ali_erenbatman@hotmail.com>
2024-02-10 18:44:56 +0300

    Deprem sonrası kısım ilk yardım çantasında olanları gösterme yapıldı

---
+++ Assets/Scripts/Player/arrangeItemForLastLevel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class arrangeItemForLastLevel : MonoBehaviour
+{
+    public GameObject[] firstkiditems;
+    void Start()
+    {
+        CheckDroppedObjectName();
+    }
+    void CheckDroppedObjectName()
+    {
+        if (PlayerPrefs.HasKey("DroppedObjectNames"))
+        {
+            string droppedObjectName = PlayerPrefs.GetString("DroppedObjectNames");
+
+            if (!string.IsNullOrEmpty(droppedObjectName))
+            {
+                Debug.Log("Dropped Object Name: " + droppedObjectName);
+                string[] droppedObjectNamesArray = droppedObjectName.Split(',');
+
+                foreach (var item in firstkiditems)
+                {
+                    if (!droppedObjectNamesArray.Contains(item.name))
+                    {
+                        item.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Dropped Object Name PlayerPrefs'te kayıtlı değil veya boş.");
+            }
+        }
+        else
+        {
+            Debug.Log("Dropped Object Name PlayerPrefs'te kayıtlı değil.");
+        }
+    }
+ 
+}
