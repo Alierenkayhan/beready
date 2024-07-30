559e7682dc6880b14805aedd0adb7ac31490eb60
Ali Eren Kayhan <a.erenkayhan@gmail.com>
2024-07-30 22:11:53 +0300

    Deprem başlamadan önce deprem birazdan başlayacak uyarısı yapılmalı

---
+++ Assets/Scripts/Managers/lvl1depremalert.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UI;
+
+public class lvl1depremalert : MonoBehaviour
+{
+    public AlertController alert;
+
+    void Start()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName == "Level 1")
+        {
+            Invoke("AlertShow", 4f);
+        }
+    }
+
+    void AlertShow()
+    {
+        alert.alert("Deprem Uyarısı",
+            "Bir süre sonra deprem başlayacak. Konumunuzu seçiminize göre belirleyin.",
+            "Tamam");
+    }
+}
